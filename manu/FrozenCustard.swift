d4e9291a5ba4ae564189ae4e9e8a49b228cde53a
kimhr <khl9566@naver.com>
2023-07-25 16:39:49 +0900

    최신 버전

--- manu/FrozenCustard.swift
+++ manu/FrozenCustard.swift
@@ -1,22 +1,5 @@
+// Foundation 프레임워크를 import 합니다.
 import Foundation
-
-class OrderDessert{
-    var name : String?
-    var price : DessertPrice?
-    
-    init(name: String? = "", price: DessertPrice? = nil) {
-        self.name = name
-        self.price = price
-    }
-    
-    func showInfo(){
-        print("name : \(name), price : \(price)원")
-    }
-    
-    func getNameAndPrice() -> (String, UInt){
-        return (name!, UInt(price!.rawValue))
-    }
-}
 
 enum DessertPrice: Int {
     case Default = 0
@@ -27,62 +10,153 @@ enum DessertPrice: Int {
     case saltedCaramel = 7000
 }
 
-class Dessert {
-    var oderDessert : OrderDessert?
+class SuperDessert : MenuProc{
+    // 질문 return을 하는 이유?
+    func displayInfo() {
+        return
+    }
     
-    func displayInfo() -> (String, UInt) {
+    func SetCountOfItem(name: String, price: Int) {
+        return
+    }
+    
+    func deleteItem(index: Int) {
+        return
+    }
+    
+    func showPriceAll() -> Int {
+        return 0
+    }
+    
+    func showDessertInfo(){
+        return
+    }
+    
+}
+
+class Dessert : SuperDessert {
+    var dessertArray : Array<Dessert> = []
+    var name : String?
+    var cnt : Int?
+    var price : Int?
+    
+    override func showDessertInfo(){
+        for i in 0..<dessertArray.count{
+            print("dessertArray[\(i)] -> 디저트 : \(dessertArray[i].name!), 가격 : \(dessertArray[i].price!), 개수 : \(dessertArray[i].cnt!)\n")
+        }
+    }
+    
+    override func SetCountOfItem(name: String, price: Int) {
+        if let dessertPrice = DessertPrice(rawValue: price){
+            switch dessertPrice{
+            // 질문 break 거는 이유?
+            case .Default :
+                print("잘못된 가격입니다.")
+                self.price = price
+                break
+            case .vanilla :
+                self.price = price
+                break
+            case .choco :
+                self.price = price
+                break
+            case .strawberry :
+                self.price = price
+                break
+            case .coffee :
+                self.price = price
+                break
+            case .saltedCaramel :
+                self.price = price
+                break
+            }
+        } else {
+            print("가격을 잘못 설정하였습니다.")
+            return
+        }
+        
+        print("개수를 입력해주세요 : ")
+        self.name = name
+        let cnt = readLine()
+        guard let cnt = cnt else {return}
+        self.cnt = Int(cnt)
+        // 질문 - 왜 self를 붙이는지??
+    }
+    
+    override func deleteItem(index: Int) {
+        if(index >= dessertArray.count){
+            print("삭제할 메뉴를 다시 선택해주세요.")
+        }else{
+            dessertArray.remove(at: index)
+            print("메뉴가 삭제되었습니다.")
+        }
+    }
+    
+    override func showPriceAll() -> Int {
+        // 질문 amount 계산 이해안됨
+        var amount = 0
+        for i in 0..<dessertArray.count {
+            amount = amount + (dessertArray[i].cnt! * dessertArray[i].price!)
+        }
+        return amount
+    }
+    
+    override func displayInfo() {
         
         print("""
 
-[Desserts MENU]
-1. vanilla Icecream | W 5.0
-2. choco Icecream | W 5.5
-3. strawberry Icecream | W 6.0
-4. coffee Icecream | W 6.5
-5. saltedCaramel Icecream | W 7.0
-0. 뒤로가기
+        [Desserts MENU]
+        1. vanilla Icecream | W 5.0
+        2. choco Icecream | W 5.5
+        3. strawberry Icecream | W 6.0
+        4. coffee Icecream | W 6.5
+        5. saltedCaramel Icecream | W 7.0
+        0. 뒤로가기
 
-""")
+        """)
         
-        let price : (String, UInt) = ("Default", 0)
+        let dessert = Dessert()
         
         let selectedStr = readLine()
         
         guard let selectedStr = selectedStr else {
             print("숫자로 입력해주세요.")
-            oderDessert = OrderDessert(name: "", price: DessertPrice.Default)
-            return oderDessert!.getNameAndPrice()
+            return
         }
-        
         let selectedNum = Int(selectedStr)
-        
         guard let selectedNum = selectedNum else {
             print("올바른 번호를 선택해주세요.")
-            oderDessert = OrderDessert(name: "", price: DessertPrice.Default)
-            return oderDessert!.getNameAndPrice()
+            return
         }
         
         switch selectedNum{
+        case 0 :
+            return
         case 1 :
             print("vanilla Icecream을 선택했습니다.")
-            oderDessert = OrderDessert(name: "vanilla Icecream", price: DessertPrice.vanilla)
+            dessert.SetCountOfItem(name: "vanilla Icecream", price: 5000)
         case 2 :
             print("choco Icecream을 선택했습니다.")
-            oderDessert = OrderDessert(name: "choco Icecream", price: DessertPrice.choco)
+            dessert.SetCountOfItem(name: "choco Icecream", price: 5500)
         case 3 :
             print("strawberry Icecream을 선택했습니다.")
-            oderDessert = OrderDessert(name: "strawberry Icecream", price: DessertPrice.strawberry)
+            dessert.SetCountOfItem(name: "strawberry Icecream", price: 6000)
         case 4 :
             print("coffee Icecream을 선택했습니다.")
-            oderDessert = OrderDessert(name: "coffee Icecream", price: DessertPrice.coffee)
+            dessert.SetCountOfItem(name: "coffee Icecream", price: 6500)
         case 5 :
             print("saltedCaramel Icecream을 선택했습니다.")
-            oderDessert = OrderDessert(name: "saltedCaramel Icecream", price: DessertPrice.saltedCaramel)
+            dessert.SetCountOfItem(name: "saltedCaramel Icecream", price: 7000)
         default :
-            print("올바른 번호를 선택해주세요.")
+            dessert.SetCountOfItem(name: "", price: 0)
+            // 질문
+        }
+        if let dessertName = dessert.name,
+           let dessertCnt = dessert.cnt,
+           let dessertPrice = dessert.price{
+            dessertArray.append(dessert)
+            // 질문 왜 if let 사용하는지
         }
         
-        
-        return oderDessert!.getNameAndPrice()
     }
 }
